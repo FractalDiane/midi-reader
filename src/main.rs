e8e57f9966dd6fdd14646bae888108e7f1059080
Diane Sparks <duncan.sprks@gmail.com>
2020-12-17 22:50:45 -0800

    Make changes on new branch (doesn't work)

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ extern crate num;
 extern crate num_derive;
 use num::FromPrimitive;
 
-#[derive(FromPrimitive)]
+/*#[derive(FromPrimitive)]
 enum ChannelEvent {
 	NoteOff = 0x8,
 	NoteOn = 0x9,
@@ -42,6 +42,53 @@ enum MetaEvent {
 	TimeSignature = 0x58,
 	KeySignature = 0x59,
 	SequencerMetaEvent = 0x7f,
+}*/
+
+enum EventClass {
+	Channel,
+	SysEx,
+	Meta,
+}
+
+#[derive(FromPrimitive)]
+enum EventType {
+	// Channel events
+	NoteOff = 0x8,
+	NoteOn = 0x9,
+	NoteAftertouch = 0xa,
+	Controller = 0xb,
+	ProgramChange = 0xc,
+	ChannelAftertouch = 0xd,
+	PitchBend = 0xe,
+
+	// Meta events
+	SequenceNumber = 0x00,
+	Text = 0x01,
+	Copyright = 0x02,
+	TrackName = 0x03,
+	InstrumentName = 0x04,
+	Lyric = 0x05,
+	Marker = 0x06,
+	CuePoint = 0x07,
+	ChannelPrefix = 0x20,
+	MidiBus = 0x21,
+	EndOfTrack = 0x2f,
+	SetTempo = 0x51,
+	SMPTEOffset = 0x54,
+	TimeSignature = 0x58,
+	KeySignature = 0x59,
+	SequencerMetaEvent = 0x7f,
+
+	// SysEx events
+	SysEx = 0x4b,
+}
+
+
+struct Event {
+	class: EventClass,
+	delta: u32,
+	status: u8,
+	params: Vec<u8>,
 }
 
 
@@ -80,62 +127,48 @@ fn read_vlq(file: &mut File, byte_count: &mut u32) -> u32 {
 }
 
 
-fn read_channel_event(event_type: u8, file: &mut File, byte_count: &mut u32) {
-	let typ: ChannelEvent = FromPrimitive::from_u8(event_type).unwrap();
-	match typ {
-		ChannelEvent::NoteOff => {
-			let note = file.read_u8().unwrap();
-			let velocity = file.read_u8().unwrap();
-			println!("Note Off\nNote: {}\nVelocity: {}\n", note, velocity);
-			*byte_count += 2;
+fn read_channel_event(delta: u32, status: u8, event_type: u8, file: &mut File, byte_count: &mut u32) -> Event {
+	let typ: EventType = FromPrimitive::from_u8(event_type).unwrap();
+	let mut params = Vec::<u8>::new();
+	let param_count = match typ {
+		EventType::NoteOff => {
+			2
+		},
+		EventType::NoteOn => {
+			2
+		},
+		EventType::NoteAftertouch => {
+			2
+		},
+		EventType::Controller => {
+			2
 		},
 
-		ChannelEvent::NoteOn => {
-			let note = file.read_u8().unwrap();
-			let velocity = file.read_u8().unwrap();
-			println!("Note On\nNote: {}\nVelocity: {}\n", note, velocity);
-			*byte_count += 2;
+		EventType::ProgramChange => {
+			1
 		},
+		EventType::ChannelAftertouch => {
+			1
+		},
+		EventType::PitchBend => {
+			2
+		},
+		_ => {
+			panic!("Attempted to read non-channel event as a channel event");
+		},
+	};
 
-		ChannelEvent::NoteAftertouch => {
-			let note = file.read_u8().unwrap();
-			let pressure = file.read_u8().unwrap();
-			println!("Note Aftertouch\nNote: {}\nPressure: {}\n", note, pressure);
-			*byte_count += 2;
-		},
-
-		ChannelEvent::Controller => {
-			let controller = file.read_u8().unwrap();
-			let value = file.read_u8().unwrap();
-			println!("Controller change\nController: {}\nValue: {}\n", controller, value);
-			*byte_count += 2;
-		},
-
-		ChannelEvent::ProgramChange => {
-			let number = file.read_u8().unwrap();
-			println!("Program change\nProgram: {}\n", number);
-			*byte_count += 1;
-		},
-
-		ChannelEvent::ChannelAftertouch => {
-			let pressure = file.read_u8().unwrap();
-			println!("Channel aftertouch\nPressure: {}\n", pressure);
-			*byte_count += 1;
-		},
-
-		ChannelEvent::PitchBend => {
-			let low = file.read_u8().unwrap() as u16;
-			let high = file.read_u8().unwrap() as u16;
-			println!("Pitch bend\nValue: {}\n", (high << 8) | low);
-			*byte_count += 2;
-		},
+	for _i in 0..param_count {
+		params.push(file.read_u8().unwrap());
+		*byte_count += 1;
 	}
+
+	Event{class: EventClass::Channel, delta, status, params}
 }
 
 
-fn read_meta_event(event_type: u8, file: &mut File, byte_count: &mut u32) {
-	let typ: MetaEvent = FromPrimitive::from_u8(event_type).unwrap();
-	let length = file.read_u8().unwrap() as u32;
+fn read_meta_event(delta: u32, event_type: u8, file: &mut File, byte_count: &mut u32) -> Event {
+	let length = file.read_u8().unwrap();
 	*byte_count += 1;
 	let mut params = Vec::<u8>::new();
 	for _i in 0..length {
@@ -143,54 +176,55 @@ fn read_meta_event(event_type: u8, file: &mut File, byte_count: &mut u32) {
 		*byte_count += 1;
 	}
 
-	println!("Type 0x{:x}", event_type);
-	println!("Length: {}", length);
-	println!("Parameters: {:?}\n", params);
+	//println!("Type 0x{:x}", event_type);
+	//println!("Length: {}", length);
+	//println!("Parameters: {:?}\n", params);
+
+	Event{class: EventClass::Meta, delta, status: event_type, params}
 }
 
 
-fn read_event(file: &mut File, byte_count: &mut u32, prev_status: &mut u8) {
+fn read_event(file: &mut File, byte_count: &mut u32, prev_status: &mut u8) -> Event {
 	let delta = read_vlq(file, byte_count);
-	println!("Delta {}", delta);
+	//println!("Delta {}", delta);
 
 	let status = file.read_u8().unwrap();
 	*byte_count += 1;
 	match status {
 		0xff => {
-			println!("Meta event");
+			//println!("Meta event");
 			let typ = file.read_u8().unwrap();
 			*byte_count += 1;
-			read_meta_event(typ, file, byte_count);
+			read_meta_event(delta, typ, file, byte_count)
 		},
 		0xf0 => {
-			println!("SysEx event");
+			//println!("SysEx event");
+			Event{class: EventClass::SysEx, delta: 0, status: 0x0, params: vec![]}
 		},
 		0xf7 => {
-			println!("SysEx event (end)");
+			//println!("SysEx event (end)");
+			Event{class: EventClass::SysEx, delta: 0, status: 0x0, params: vec![]}
 		},
 		_ => {
-			println!("Normal event");
+			//println!("Normal event");
 
 			let typ: u8;
-			let channel: u8;
 			// Running status:
 			// If the high bit isn't set, we're reusing the same
 			// status byte from the last event.
 			if status < 0x80 {
 				typ = high4(*prev_status);
-				channel = low4(*prev_status);
 				file.seek(SeekFrom::Current(-1)).unwrap();
 				*byte_count -= 1;
 			}
 			else {
 				typ = high4(status);
-				channel = low4(status);
 				*prev_status = status;
 			}
 
-			println!("Type: 0x{:x}\nChannel: {}", typ, channel);
+			//println!("Type: 0x{:x}\nChannel: {}", typ, channel);
 
-			read_channel_event(typ, file, byte_count);
+			read_channel_event(delta, typ, if status < 0x80 {*prev_status} else {status}, file, byte_count)
 		},
 	}
 }
@@ -223,9 +257,12 @@ fn main() {
 	else {
 		println!("Time division: {} frames/second\n", time & 0x7fff);
 	}
-	
+
+	let mut events = Vec::<Vec::<Event>>::new();
+
 	for track in 0..tracks {
-		println!("Track {}", track);
+		//println!("Track {}", track);
+		events.push(Vec::<Event>::new());
 
 		let this_chunk_id = read_string(&mut file, 4);
 		if this_chunk_id != "MTrk" {
@@ -234,12 +271,12 @@ fn main() {
 		}
 
 		let this_chunk_size = file.read_u32::<BigEndian>().unwrap();
-		println!("{} bytes\n", this_chunk_size);
+		//println!("{} bytes\n", this_chunk_size);
 
 		let mut byte_count = 0;
 		let mut status: u8 = 0x0;
 		while byte_count < this_chunk_size {
-			read_event(&mut file, &mut byte_count, &mut status);
+			events[track as usize].push(read_event(&mut file, &mut byte_count, &mut status));
 		}
 	}
 }
